ce0b44326cf6b1ab322f2349581e88de2b3404b7
moa2027 <98979565+moa2027@users.noreply.github.com>
2022-04-15 13:58:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -114,10 +114,11 @@ def ping(host, timeout=1):
         delay = doOnePing(dest, timeout)
         print(delay)
         time.sleep(1)  # one second
-        
+        DArr[i]=delay
     #You should have the values of delay for each ping here; fill in calculation for packet_min, packet_avg, packet_max, and stdev
-    #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
-
+    packet_min= Min(DArr)
+    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
+    print(packet_min)    
     return vars
 
 if __name__ == '__main__':
